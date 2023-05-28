024a54211fe616abb7dd6540f5d4f8680f59622f
Yakub Kristianto <yakub1986@gmail.com>
2023-05-29 04:03:35 +0700

    Add width, height & class property

--- Icon.svelte
+++ Icon.svelte
@@ -1,5 +1,7 @@
 <script>
     export let icon = ""
+    export let width = "48"
+    export let height = "48"
     let req = getIcon(icon)
     let res = undefined
     req.then(out => {
@@ -12,7 +14,7 @@
 </script>
 
 {#if res !== undefined}
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" width=48 height=48 viewBox="0 0 {res.width} {res.height}" class="iconify iconify--mdi-light">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" {width} {height} viewBox="0 0 {res.width} {res.height}" class="iconify iconify--mdi-light {$$props.class}">
     {@html res.path}
 </svg>
 {/if}
